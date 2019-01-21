7e525360cac5689ee57bcc70d7761ec3c910b46f
Ad-Tripper <adnanedriouche98@gmail.com>
2019-01-21 16:21:05 +0000

    adding a multiplication function

--- calcutor.cpp
+++ calcutor.cpp
@@ -5,12 +5,22 @@ double sum(double a ,double b ) {
 
 }
 
+double mul(double a , double b) {
+
+	return(a*b) ; 
+}
+
 
 
 
 in main () {
 	
+ double a , b ; 
 
-
+ cout<<"enter two numbers "<<endl ; 
+ cin>>a ;
+ cin>>b ;
+ cout<<"the sum is :"<<sum(a,b) << endl ;     
+return(0) ; 
 
 } 
